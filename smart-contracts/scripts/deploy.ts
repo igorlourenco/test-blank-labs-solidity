eb1655b35638ca600508df5ab2615339901bfa49
igorlourenco <igorlourenco.dev@gmail.com>
2025-01-16 11:03:00 -0300

    feat: create deployment script

---
+++ smart-contracts/scripts/deploy.ts
@@ -0,0 +1,22 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const BLTM = await ethers.getContractFactory("BLTM");
+  const bltm = await BLTM.deploy(deployer.address);
+
+  await bltm.waitForDeployment();
+
+  const address = await bltm.getAddress();
+  console.log("BLTM token deployed to:", address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  }); 
