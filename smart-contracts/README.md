655e8a73614c042a98e423a315003056c2223f3c
igorlourenco <igorlourenco.dev@gmail.com>
2025-01-16 11:10:58 -0300

    feat: deploy BLTM

--- smart-contracts/README.md
+++ smart-contracts/README.md
@@ -11,3 +11,6 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat ignition deploy ./ignition/modules/Lock.ts
 ```
+
+
+BLTM token deployed to: 0x0DA8D7B57e5905c793Eb873B89bb44Ca18eddBe1
--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config'
+
+const DEPLOYER_ACCOUNT = process.env.DEPLOYER_ACCOUNT ?? ''
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+    networks: {
+    polygon_amoy: {
+      url: "https://rpc-amoy.polygon.technology",
+      accounts: [DEPLOYER_ACCOUNT]
+    }
+  }
 };
 
 export default config;
